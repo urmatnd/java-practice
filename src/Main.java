0c6e961a320470fe3ecdb29821fcf3b0b85d1033
Urmatbek Dubanaev <urmatnd@gmail.com>
2025-03-13 18:03:12 +0600

    ask temperature f

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Выбор одежны по температуре воздуха
+        System.out.println("Аба ырайы канча градус корсотуп жатат?");
+        int temperature = scanner.nextInt();
+        if (temperature < 0){
+            System.out.println("Кун суук, жылуу кийин.");
+        }
+        else if (temperature <= 30){
+            System.out.println("Кундогудой кийин.");
+        }
+        else {
+            System.out.println("Кун ысык. Женил кийин.");
+        }
     }
 }
